045f8768b53fa6e99b3532d20a06a39a26b46ca8
unknown <dlgur9612@gmail.com>
2022-05-15 22:41:06 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/controller/WebController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class WebController {
+
+    @GetMapping("/main")
+    public String main(){
+        return "main";
+    }
+
+}
+
---
+++ src/main/webapp/WEB-INF/jsp/main.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: PC
+  Date: 2022-05-15
+  Time: PM 10:16
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>test</title>
+</head>
+<body>
+    <h1>hI!</h1>
+</body>
+</html>
